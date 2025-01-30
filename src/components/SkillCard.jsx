c53e18ba4d67fd20b5496af5c71ad096562f51f1
BehzadHashemian <shashe43@uwo.ca>
2025-01-30 17:59:42 -0500

    Update SkillCard.jsx

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SkillCard = ({ skills , name , img}) => {
     return (
-        <div className='px-4 py-6 md:w-[45%] lg:w-[30%] border-2 rounded-2xl border-blue-300 shadow-lg'>
+        <div className='px-4 py-6 border-2 rounded-4xl border-blue-300 shadow-lg'>
             <div className='flex px-2 justify-center'>
                 <img className='w-[10%] rounded-full mx-4 mb-4' src={img} alt="" />
                 <div className='text-md '>{name}</div>
@@ -10,7 +10,7 @@ const SkillCard = ({ skills , name , img}) => {
             
             {skills.map((skill, index) => (
                 <div key={index} className='flex my-2 justify-between items-center'>
-                    <div>{skill.name}</div>
+                    <div className='text-sm'>{skill.name}</div>
                     <div className='bg-gray-800 rounded-full h-2.5 w-[70%]'>
                         <div
                             className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full'
