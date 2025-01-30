a4ee18d8dc5e1cc0b0c676af2eb84d9d61e02d38
BehzadHashemian <shashe43@uwo.ca>
2025-01-30 17:59:44 -0500

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,12 +10,58 @@ const Skills = () => {
         { name: "CSS", level: 90 },
         { name: "Angular", level: 80 }
     ]
+    const databases = [
+        { name: "SQL Server", level: 100 },
+        { name: "MySQL", level: 100 },
+        { name: "PostgreSQL", level: 100 },
+        { name: "SSIS", level: 90 },
+        { name: "SSAS", level: 80 },
+        { name: "ETL", level: 90 }
+    ];
+
+    const cloudAndContainerTech = [
+        { name: "Google Cloud", level: 90 },
+        { name: "AWS", level: 85 },
+        { name: "Docker", level: 95 },
+        { name: "Kubernetes", level: 90 }
+    ];
+
+    const mobileAndUIUX = [
+        { name: "Kotlin", level: 90 },
+        { name: "Java", level: 70 },
+        { name: "React Native", level: 80 },
+        { name: "UI/UX", level: 80 },
+        { name: "Material UI", level: 85 },
+        { name: "Jetpack", level: 100 }
+    ];
+    const pythonAndML = [
+        { name: "Python", level: 90 },
+        { name: "TensorFlow", level: 80 },
+        { name: "Pandas", level: 85 },
+        { name: "Numpy", level: 85 }
+    ];
+
+
 
     return (
-        <div className='bg-black text-white flex justify-center px-10 py-10'>
-            <SkillCard skills={frontend} name="Front-End Development" img={pic}/>
+        <div className='bg-black text-white flex-col ' >
+            <div className='text-6xl text-center'>
+                Skills
+            </div>
+            <div className='bg-black text-white grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-10 py-10'>
+                <SkillCard skills={frontend} name="Front-End Development" img={pic} />
+                <SkillCard skills={databases} name="Database Management" img={pic} />
+                <SkillCard skills={cloudAndContainerTech} name="Cloud & Container Technologies" img={pic} />
+                <SkillCard skills={mobileAndUIUX} name="Mobile Development" img={pic} />
+                <SkillCard skills={pythonAndML} name="Machine Learning" img={pic} />
+
+
+            </div>
         </div>
-    )
+
+    );
+
+
 }
 
 export default Skills
