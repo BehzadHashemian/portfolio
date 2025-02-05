868a6b18c4acd6d44b8e56067b22d66c136a260e
BehzadHashemian <shashe43@uwo.ca>
2025-02-05 05:06:55 -0500

    add project detail to card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,20 +1,37 @@
 import React from 'react'
-
+import { useState } from 'react'
+import ProjectDetail from './ProjectDetail'
 const ProjectCard = (props) => {
-  return (
-    <div className='flex-col'>
-        <div className='flex items-center'>
-            <img className='w-10' src={props.img} alt="" />
-            <div>{props.name}</div>
+    const [ShowDetail, setShowDetail] = useState(false)
+    const detailpopup = () => {
+        if (ShowDetail) {
+            return (
+                <ProjectDetail
+                    name={props.name}
+                    img={props.img}
+                    desc={props.desc}
+                    loc={props.loc}
+                    onClose={() => setShowDetail(false)}
+                />
+            );
+        }
+        return null;
+    }
+    return (
+        <div className='flex flex-col border-2 rounded-4xl border-blue-300 p-6'>
+            <div className='flex flex-col items-center text-center'>
+                <img className='w-[100%] rounded-2xl' src={props.img} alt="" />
+                <div className='text-xl font-semibold mt-4'>{props.name}</div>
+
+            </div>
+            <div className='flex justify-center mt-8'>
+                <button className='border-2 p-2 rounded-3xl' onClick={() => setShowDetail(!ShowDetail)}>Show Details</button>
+            </div>
+            {detailpopup()}
         </div>
-        <div className='text-gray-400 italic'>
-            {props.loc}
-        </div>
-        <div>
-            {props.desc}
-        </div>
-    </div>
-  )
+
+
+    )
 }
 
 export default ProjectCard
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,35 +1,32 @@
 import React from 'react'
 import ProjectCard from './ProjectCard'
-import proimg from '../assets/profile.jpeg'
+import proimg from '../assets/sql.webp'
 const Projects = () => {
 
     const projectsData = [
         {
           name: "Rental Movie Data Warehouse",
-          img: proimg, // You can replace this with the relevant image for the project.
-          desc: `Built a data warehouse using SQL Server for a rental movie system.
-                 Developed ETL processes using Microsoft Visual Studio (SSIS) to 
-                 integrate and transform data from operational database sources. 
-                 Utilized SQL Server Analysis Services (SSAS) for advanced data visualization 
-                 and interactive exploration of rental movie data.`,
+          img: proimg, 
+          desc: [
+            "Built a data warehouse using SQL Server for a rental movie system.",
+            "Developed ETL processes using Microsoft Visual Studio (SSIS) to integrate and transform data from operational database sources.",
+            "Utilized SQL Server Analysis Services (SSAS) for advanced data visualization and interactive exploration of rental movie data."
+          ],
           loc: "Western University",
         },
         {
           name: "Olympic History Database",
-          img: proimg, // Replace this with an image specific to this project, if available.
-          desc: `Designed and implemented a PostgreSQL database for efficient storage and
-                 retrieval of Olympic history data.
-                 Created and managed an Olympic history data warehouse, implementing an
-                 ETL pipeline using Airflow and Droid for efficient population and
-                 management.
-                 Utilized Superset for advanced data visualization, offering an interactive
-                 platform to explore and present analyzed Olympic history data.
-                 Leveraged advanced data analysis techniques, including machine learning 
-                 with Apache Spark, to uncover valuable insights within the Olympic history data.`,
+          img: proimg, 
+          desc: [
+            "Designed and implemented a PostgreSQL database for efficient storage and retrieval of Olympic history data.",
+            "Created and managed an Olympic history data warehouse, implementing an ETL pipeline using Airflow and Droid for efficient population and management.",
+            "Utilized Superset for advanced data visualization, offering an interactive platform to explore and present analyzed Olympic history data.",
+            "Leveraged advanced data analysis techniques, including machine learning with Apache Spark, to uncover valuable insights within the Olympic history data."
+          ],
           loc: "Western University",
         },
-        // Add more projects here if needed
       ];
+      
       
 
 
@@ -39,10 +36,10 @@ const Projects = () => {
 
     return (
         <div className='bg-black text-white flex-col justify-center'>
-            <div className='text-6xl text-center'>
+            <div className='text-6xl text-center py-6 pb-10'>
                 Projects
             </div>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                 {projectsData.map((project, index) => (
                     <ProjectCard
                         key={index}
