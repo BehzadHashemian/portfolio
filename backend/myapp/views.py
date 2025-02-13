78bc6c5baebc78114831d934df61ccb5de5c7a58
BehzadHashemian <shashe43@uwo.ca>
2025-02-12 21:03:35 -0500

    create view for skills

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from . models import Skill
+from .serializers import SkillSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
+@api_view(['GET'])
+def getskills(request):
+    skills = Skill.objects.all()
+    serilizer = SkillSerializer(skills)
+    return Response(serilizer.data)
