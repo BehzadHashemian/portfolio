be269ef93b0aded1ee757fd1649efccb6abca25e
BehzadHashemian <shashe43@uwo.ca>
2025-02-13 00:10:33 -0500

    create view for Project model

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -1,5 +1,5 @@
-from . models import Skill
-from .serializers import SkillSerializer
+from . models import Skill, Project
+from .serializers import SkillSerializer, ProjectSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 # Create your views here.
@@ -7,4 +7,9 @@ from rest_framework.response import Response
 def getskills(request):
     skills = Skill.objects.all()
     serilizer = SkillSerializer(skills , many = True)
+    return Response(serilizer.data)
+
+def getproject(request):
+    projects = Project.objects.all()
+    serilizer = ProjectSerializer(projects , many = True)
     return Response(serilizer.data)
